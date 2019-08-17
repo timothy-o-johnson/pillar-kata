af5dd8fb89475225301d549f567dcf37418a7b06
Tim <pavementii@gmail.com>
2019-08-17 19:12:11 -0400

    [fc] reflect an increase in per-unit prices after several scans

--- checkout-order-total.js
+++ checkout-order-total.js
@@ -1,23 +1,42 @@
-function checkoutOrderTotal (entry, details) {
-  let result = ''
+function CheckoutOrderApp () {
+  this.itemList = {}
+  this.basket = {}
 
-  if (entry === 'add') {
-    result = 'Items:\n'
-    let itemList = Array.isArray(details) ? details : [details]
+  this.scanItems = function (items) {
+    // make sure the scans are in an array
+    items = Array.isArray(items) ? items : [items]
+    let totalPrice = 0
+    let basket = this.basket
 
-    itemList.forEach(item => {
-      result += `${item.name} @ $${item.price}\n`
+    items.forEach(item => {
+      if (basket[item]) {
+        basket[item]++
+      } else {
+        basket[item] = 1
+      }
     })
 
-    console.log(result)
-  } else {
-    result = "$1.89"
+    Object.keys(basket).forEach(item => {
+      totalPrice += basket[item] * this.itemList[item]
+    })
+
+    return totalPrice
   }
-  
-  
-  return result
+
+  this.configurePrices = function (items) {
+    let result = 'Items:\n'
+    items = Array.isArray(items) ? items : [items]
+
+    items.forEach(item => {
+      result += `${item.name} @ $${item.price}\n`
+      this.itemList[item.name] = item.price
+    })
+
+    console.log(result, this.itemList)
+    return result
+  }
 }
 
 module.exports = {
-  checkoutOrderTotal
+  CheckoutOrderApp
 }
--- checkout-order-total.test.js
+++ checkout-order-total.test.js
@@ -1,28 +1,40 @@
-const checkoutOrderTotal = require('./checkout-order-total')
+let CheckoutOrderApp = require('./checkout-order-total').CheckoutOrderApp
+
+let checkoutOrderApp = new CheckoutOrderApp()
+
 
 describe('accepts a scanned item', () => {
   test('configure the price of a per-unit item', () => {
-    const entry = "add"
+    // const entry = "add"
     const item = {name: 'soup', price: 1.89}
     const itemList = `Items:\nsoup @ $1.89`
 
-    expect(checkoutOrderTotal.checkoutOrderTotal(entry, item)).toMatch(itemList)
+    expect(checkoutOrderApp.configurePrices(item)).toMatch(itemList)
+
   })
 
   test('configure the price of several per-unit items', () => {
-    const entry = "add"
+    // const entry = "add"
     const soup = {name: 'soup', price: 1.89}
     const sardines = {name: 'sardines', price: 0.89}
     const items = [soup, sardines]
     const itemList = `Items:\nsoup @ $1.89\nsardines @ $0.89`
 
-    expect(checkoutOrderTotal.checkoutOrderTotal(entry, items)).toMatch(itemList)
+    expect(checkoutOrderApp.configurePrices(items)).toMatch(itemList)
   })
 
   test('reflect an increase in the per-unit price after a scan', () => {
     const entry = "soup"
-    const total = "$1.89"
+    const total = 1.89
 
-    expect(checkoutOrderTotal.checkoutOrderTotal(entry)).toMatch(total)
+    expect(checkoutOrderApp.scanItems(entry)).toEqual(total)
+  })
+
+  test('reflect an increase in the per-unit price after several scans', () => {
+    const entry = ["soup","soup"]
+    const total = 1.89 * 2
+    checkoutOrderApp.basket = {} //empty the basket
+ 
+    expect(checkoutOrderApp.scanItems(entry)).toEqual(total)
   })
 })
