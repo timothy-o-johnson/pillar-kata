7f6111f6e222f81167f500776cb1afd7126af562
Tim <pavementii@gmail.com>
2019-08-17 22:02:19 -0400

    [fc] total reflects an increase by an item's given weight

--- checkout-order-total.js
+++ checkout-order-total.js
@@ -1,6 +1,8 @@
 function CheckoutOrderApp () {
   this.itemList = {}
   this.basket = {}
+  let price = 0
+  let quantity = 0
 
   this.scanItemsAndReturnTotalPrice = function (items) {
     items = this.makeArray(items)
@@ -9,16 +11,20 @@ function CheckoutOrderApp () {
 
     // add items to basket and update the quantity of each item
     items.forEach(item => {
-      if (basket[item]) {
-        basket[item]++
+      
+      if (Array.isArray(item)) {
+        this.loadBasketItemByWeight(item)
       } else {
-        basket[item] = 1
+        this.loadBasketItemByUnit(item)
       }
     })
 
     // calculate totalPrice
     Object.keys(basket).forEach(item => {
-      totalPrice += basket[item] * this.itemList[item]
+      quantity = basket[item]
+      price = this.itemList[item]
+
+      totalPrice +=  price * quantity /*?+*/
     })
 
     return totalPrice
@@ -26,22 +32,61 @@ function CheckoutOrderApp () {
 
   this.configurePricesAndReturnAnItemsList = function (items) {
     let result = 'Items:\n'
-    items = this.makeArray(items)
+    items = this.makeArray(items) /*?+*/
 
     items.forEach(item => {
       result += `${item.name} @ $${item.price}\n`
       this.itemList[item.name] = item.price
     })
 
-  
     return result
   }
 
-  this.makeArray = function (arrayData){
+  this.makeArray = function (arrayData) {
     return Array.isArray(arrayData) ? arrayData : [arrayData]
+  }
+
+  this.loadBasketItemByWeight = function (item) {
+    console.log(item)
+    let basket = this.basket
+    let itemName = item[0] /*?+*/
+    let itemWeight = parseFloat(item[1]) /*?+*/
+
+    if (basket[itemName]) {
+      basket[itemName] += itemWeight
+    } else {
+      basket[itemName] = itemWeight
+    }
+
+    basket
+  }
+
+  this.loadBasketItemByUnit = function (item) {
+    let basket = this.basket
+
+    if (basket[item]) {
+      basket[item]++
+    } else {
+      basket[item] = 1
+    }
   }
 }
 
 module.exports = {
   CheckoutOrderApp
 }
+
+let checkoutOrderApp = new CheckoutOrderApp()
+// per-unit items
+const soup = { name: 'soup', price: 1.89 }
+const sardines = { name: 'sardines', price: 0.89 }
+const groundBeef = { name: 'ground beef', price: 2.5, byWeight: true }
+const bananas = { name: 'bananas', price: 0.25, byWeight: true }
+
+const items = [soup, sardines, bananas]    
+
+const scans = ['soup', 'soup', ['bananans', 1.3]]
+const total = 1.89 * 2 + 0.25 * 1.3
+
+checkoutOrderApp.configurePricesAndReturnAnItemsList(items)
+let totalPrice = checkoutOrderApp.scanItemsAndReturnTotalPrice(scans) /*?+*/
--- checkout-order-total.test.js
+++ checkout-order-total.test.js
@@ -2,22 +2,19 @@ let CheckoutOrderApp = require('./checkout-order-total').CheckoutOrderApp
 
 let checkoutOrderApp = new CheckoutOrderApp()
 // per-unit items
-const soup = {name: 'soup', price: 1.89}
-const sardines = {name: 'sardines', price: 0.89}
-const groundBeef = {name: 'ground beef', price: 2.50, byWeight: true}
-const bananas = {name: 'bananas', price: .25, byWeight: true}
+const soup = { name: 'soup', price: 1.89 }
+const sardines = { name: 'sardines', price: 0.89 }
+const groundBeef = { name: 'ground beef', price: 2.5, byWeight: true }
+const bananas = { name: 'bananas', price: 0.25, byWeight: true }
 
 describe('Use Case #1: accepts a scanned item', () => {
-
-  test('configure the price of a per-unit item', () => {    
+  test('configure the price of a per-unit item', () => {
     const itemList = `Items:\nsoup @ $1.89`
 
     expect(checkoutOrderApp.configurePricesAndReturnAnItemsList(soup)).toMatch(itemList)
-
   })
 
   test('configure the price of several per-unit items', () => {
-    
     const items = [soup, sardines]
     const itemList = `Items:\nsoup @ $1.89\nsardines @ $0.89`
 
@@ -25,34 +22,34 @@ describe('Use Case #1: accepts a scanned item', () => {
   })
 
   test('total reflects an increase by the per-unit price after a scan', () => {
-    const scan = "soup"
+    const scan = 'soup'
     const total = 1.89
 
     expect(checkoutOrderApp.scanItemsAndReturnTotalPrice(scan)).toEqual(total)
   })
 
   test('total reflects an increase by the per-unit price after several scans', () => {
-    checkoutOrderApp.basket = {} //empty the basket
-    const scans = ["soup","soup"]
+    checkoutOrderApp.basket = {} // empty the basket
+    const scans = ['soup', 'soup']
     const total = 1.89 * 2
-    
+
     expect(checkoutOrderApp.scanItemsAndReturnTotalPrice(scans)).toEqual(total)
   })
 })
 
 describe('Use Case #2: accepts a scanned item and weight', () => {
   test('configure the price of a by-weight item', () => {
-    checkoutOrderApp.basket = {} //empty the basket   
-    const items = [soup, sardines, bananas]    
+    checkoutOrderApp.basket = {} // empty the basket
+    const items = [soup, sardines, bananas]
     const itemList = `Items:\nsoup @ $1.89\nsardines @ $0.89\nbananas @ $0.25`
 
     expect(checkoutOrderApp.configurePricesAndReturnAnItemsList(items)).toMatch(itemList)
   })
 
-  // test('total reflects an increase of the price of the item for the given weight', () => {    
-  //   const itemList = `Items:\nsoup @ $1.89`
+  test('total reflects an increase by an item\'s given weight', () => {
+    const scans = ['soup', 'soup', ['bananas', 1.3], ['bananas', 5], 'sardines', 'sardines']
+    const total = (1.89 * 2) + (0.25 * 1.3) + (.25 * 5) + (.89 * 2)
 
-  //   expect(checkoutOrderApp.configurePricesAndReturnAnItemsList(soup)).toMatch(itemList)
-
-  // })
+    expect(checkoutOrderApp.scanItemsAndReturnTotalPrice(scans)).toEqual(total)
+  })
 })
